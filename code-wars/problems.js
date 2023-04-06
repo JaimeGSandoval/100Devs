d937cc4b8486ba3b83a93fb2d4e1788969f40758
JaimeGSandoval <jaime.devs@gmail.com>
2023-04-06 13:30:41 -0700

    code-wars/

--- code-wars/problems.js
+++ code-wars/problems.js
@@ -160,3 +160,13 @@ function capitals(word) {
 
   return result;
 }
+
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+function solution2(str, ending) {
+  return str.substr(-ending.length) == ending;
+}
